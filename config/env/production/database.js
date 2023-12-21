9aaddb799bf88f91d54da3dfe0289edaa631ff54
Harsh00700 <harshgandhi90095@gmail.com>
2023-12-22 02:36:54 +0530

    Database Commit

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi-demo'),
+        host: env('DATABASE_HOST', 'strapi-demo-do-user-15423972-0.c.db.ondigitalocean.com'),
+        port: env.int('DATABASE_PORT', 25060),
+        database: env('DATABASE_NAME', 'defaultdb'),
         user: env('DATABASE_USERNAME', 'doadmin'),
         password: env('DATABASE_PASSWORD', 'AVNS_jYJu8Kc9yaS6aqzo4J9'),
         ssl: {
